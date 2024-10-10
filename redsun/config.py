2cc85bdfdbd8c8b361611b531a74541886f6a98a
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-10-10 17:37:33 +0200

    Modify docstring, default setup set to `None`

--- redsun/config.py
+++ redsun/config.py
@@ -15,25 +15,27 @@ class RedSunInstanceInfo:
     This class is used to store the configuration of a running RedSun application;
     it provides information about the hardware layout and the selected acquisition engine.
     All hardware models must be coherent with the selected acquisition engine.
+
+    A minimal configuration should include the selected acquisition engine.
     """
 
-    engine : Optional[str] = AcquisitionEngineTypesEnum.EXENGINE
+    engine : str = AcquisitionEngineTypesEnum.EXENGINE
     """ Acquisition engine selected for the current instance.
     Defaults to 'exengine'.
     """
 
-    detectors : Dict[str, DetectorModelInfo]
+    detectors : Optional[Dict[str, DetectorModelInfo]] = None
     """ Detector model informations dictionary.
     """
 
-    lights : Dict[str, LightModelInfo]
+    lights : Optional[Dict[str, LightModelInfo]] = None
     """ Light source model informations dictionary.
     """
 
-    motors : Dict[str, MotorModelInfo]
+    motors : Optional[Dict[str, MotorModelInfo]] = None
     """ Motor model informations dictionary.
     """
 
-    scanners : Dict[str, ScannerModelInfo]
+    scanners : Optional[Dict[str, ScannerModelInfo]] = None
     """ Scanner model informations dictionary.
     """
