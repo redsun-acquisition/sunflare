63d63d5a1c4fded0ea51b6d87b55476cbd65dccd
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-10-14 18:11:44 +0200

    Implement base class
    - add enumerator for device types;
    - add base method `register_device`

--- redsun/engine/registry.py
+++ redsun/engine/registry.py
@@ -1,9 +1,28 @@
 from typing import TYPE_CHECKING
 from abc import ABC, abstractmethod
+from enum import Enum
 
 if TYPE_CHECKING:
     from redsun.toolkit.virtualbus import VirtualBus
     from redsun.config import RedSunInstanceInfo
+    from typing import Any, Dict
+
+class DeviceTypes(str, Enum):
+    """ Supported device types in the device registry.
+    
+    Parameters
+    ----------
+    MOTOR : str
+        Motor device.
+    LIGHT : str
+        Light source device.
+    SCANNER : str
+        Scanner device.
+    """
+    DETECTOR : str = 'detector'
+    MOTOR : str = 'motor'
+    LIGHT : str = 'light'
+    SCANNER : str = 'scanner'
 
 class DeviceRegistry(ABC):
     """ `DeviceRegistry` abstract base class.
@@ -35,7 +54,64 @@ class DeviceRegistry(ABC):
         Inter-module virtual bus.
     """
 
+    @abstractmethod
     def __init__(self, config_options: "RedSunInstanceInfo", virtual_bus: "VirtualBus", module_bus: "VirtualBus"):
         self._config = config_options
         self._virtual_bus = virtual_bus
         self._module_bus = module_bus
+    
+    @abstractmethod
+    def register_device(name: str, category: DeviceTypes, device: "Any") -> None:
+        """ Add a new device to the registry. 
+        
+        Child classes must implement this method to add a new device to the registry.
+
+        Parameters
+        ----------
+        name : str
+            Device unique identifier.
+        category : DeviceTypes
+            Device type.
+        device : Any
+            Device instance \\
+            The device instance must be coherent with the selected acquisition engine.\\
+            This means that devices to be added to the registry must inherit from the correct device model class for the selected engine.
+        """
+        ...
+    
+    @abstractmethod
+    @property
+    def detectors(self) -> Dict[str, "Any"]:
+        """ Detectors dictionary.
+        
+        Device class type is engine-specific and must inherit from `DetectorModel`.
+        """
+        ...
+
+    @abstractmethod
+    @property
+    def motors(self) -> Dict[str, "Any"]:
+        """ Motors dictionary.
+        
+        Device class type is engine-specific and must inherit from `MotorModel`.
+        """
+        ...
+    
+    @abstractmethod
+    @property
+    def lights(self) -> Dict[str, "Any"]:
+        """ Lights dictionary.
+        
+        Device class type is engine-specific and must inherit from `LightModel`.
+        """
+        ...
+    
+    @abstractmethod
+    @property
+    def scanners(self) -> Dict[str, "Any"]:
+        """ Scanners dictionary.
+        
+        Device class type is engine-specific and must inherit from `ScannerModel`.
+        """
+        ...
+    
