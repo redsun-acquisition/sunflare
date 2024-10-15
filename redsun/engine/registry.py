eb94f3d1518a180e42c9587d875e65394b83802b
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-10-15 14:25:45 +0200

    Better docstring and type check

--- redsun/engine/registry.py
+++ redsun/engine/registry.py
@@ -7,6 +7,8 @@ if TYPE_CHECKING:
     from redsun.config import RedSunInstanceInfo
     from typing import Any, Dict
 
+__all__ = ['DeviceTypes', 'DeviceRegistry']
+
 class DeviceTypes(str, Enum):
     """ Supported device types in the device registry.
     
@@ -52,6 +54,17 @@ class DeviceRegistry(ABC):
     
     module_bus : VirtualBus
         Inter-module virtual bus.
+    
+    Properties
+    ----------
+    detectors : Dict[str, Any]
+        Detectors dictionary. Device class type is engine-specific and must inherit from `DetectorModel`.
+    Motors : Dict[str, Any]
+        Motors dictionary. Device class type is engine-specific and must inherit from `MotorModel`.
+    Lights : Dict[str, Any]
+        Lights dictionary. Device class type is engine-specific and must inherit from `LightModel`.
+    Scanners : Dict[str, Any]
+        Scanners dictionary. Device class type is engine-specific and must inherit from `ScannerModel`.
     """
 
     @abstractmethod
@@ -61,7 +74,7 @@ class DeviceRegistry(ABC):
         self._module_bus = module_bus
     
     @abstractmethod
-    def register_device(name: str, category: DeviceTypes, device: "Any") -> None:
+    def register_device(name: str, category: "DeviceTypes", device: "Any") -> None:
         """ Add a new device to the registry. 
         
         Child classes must implement this method to add a new device to the registry.
@@ -81,37 +94,20 @@ class DeviceRegistry(ABC):
     
     @abstractmethod
     @property
-    def detectors(self) -> Dict[str, "Any"]:
-        """ Detectors dictionary.
-        
-        Device class type is engine-specific and must inherit from `DetectorModel`.
-        """
+    def detectors(self) -> "Dict[str, Any]":
         ...
 
     @abstractmethod
     @property
-    def motors(self) -> Dict[str, "Any"]:
-        """ Motors dictionary.
-        
-        Device class type is engine-specific and must inherit from `MotorModel`.
-        """
+    def motors(self) -> "Dict[str, Any]":
         ...
     
     @abstractmethod
     @property
-    def lights(self) -> Dict[str, "Any"]:
-        """ Lights dictionary.
-        
-        Device class type is engine-specific and must inherit from `LightModel`.
-        """
+    def lights(self) -> "Dict[str, Any]":
         ...
     
     @abstractmethod
     @property
-    def scanners(self) -> Dict[str, "Any"]:
-        """ Scanners dictionary.
-        
-        Device class type is engine-specific and must inherit from `ScannerModel`.
-        """
+    def scanners(self) -> "Dict[str, Any]":
         ...
-    
