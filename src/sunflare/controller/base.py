661d6fa3e31ea7436d5d1d7e00db1860c3b95d49
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-10-21 15:23:38 +0200

    Update base controllers

--- src/sunflare/controller/base.py
+++ src/sunflare/controller/base.py
@@ -6,10 +6,12 @@ from redsun.toolkit.utils import create_evented_dataclass
 from redsun.toolkit.virtualbus import VirtualBus
 from redsun.toolkit.log import Loggable
 if TYPE_CHECKING:
-    from typing import Any
+    from typing import Any, Iterable, Generator, Union
     from redsun.toolkit.virtualbus import VirtualBus
     from redsun.toolkit.config import ControllerInfo
     from redsun.toolkit.engine import DeviceRegistry
+
+__all__ = ['DeviceController', 'ComputationalController']
 
 class BaseController(ABC, Loggable):
     """ Base controller class. Implements `Loggable` protocol.
@@ -48,6 +50,17 @@ class BaseController(ABC, Loggable):
 
 class DeviceController(BaseController):
     """ Device controller base class.
+
+    Parameters
+    ----------
+    ctrl_info : ControllerInfo
+        Controller information dataclass.
+    dev_registry : DeviceRegistry
+        Device registry API.
+    virtual_bus : VirtualBus
+        Intra-module virtual bus.
+    module_bus : VirtualBus
+        Inter-module virtual bus.
     """
 
     @abstractmethod
@@ -57,4 +70,42 @@ class DeviceController(BaseController):
                  virtual_bus: VirtualBus, 
                  module_bus: VirtualBus) -> None:
         super().__init__(ctrl_info, dev_registry, virtual_bus, module_bus)
+    
+    # TODO: add APIs...
+
+class ComputationalController(BaseController):
+    """ Computational controller base class.
+
+    Parameters
+    ----------
+    ctrl_info : ControllerInfo
+        Controller information dataclass.
+    dev_registry : DeviceRegistry
+        Device registry API.
+    virtual_bus : VirtualBus
+        Intra-module virtual bus.
+    module_bus : VirtualBus
+        Inter-module virtual bus.
+    """
+
+    @abstractmethod
+    def __init__(self, 
+                 ctrl_info: ControllerInfo, 
+                 dev_registry: DeviceRegistry, 
+                 virtual_bus: VirtualBus, 
+                 module_bus: VirtualBus) -> None:
+        super().__init__(ctrl_info, dev_registry, virtual_bus, module_bus)
+    
+    @abstractmethod
+    def register_workflow(self, workflow: "Union[Iterable, Generator]") -> None:
+        """ Submits a new available workflow.
+
+        A workflow can be a generator or an iterable object constructed within the controller.
+
+        Parameters
+        ----------
+        workflow : Union[Iterable, Generator]
+            Workflow generator or iterable.
+        """
+        ...
     
