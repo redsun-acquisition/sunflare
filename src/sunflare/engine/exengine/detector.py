e2947764b0d296ef464bcdaa89f7270cb8ed494c
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-01 14:09:15 +0100

    Post mypy fix
    - dependency on `exengine` package causes mypy to fail;
    - file will be avoided on mypy checks if necessary

--- src/sunflare/engine/exengine/detector.py
+++ src/sunflare/engine/exengine/detector.py
@@ -3,11 +3,12 @@ from typing import Tuple, TYPE_CHECKING
 from redsun.toolkit.engine.detector import DetectorModel
 from exengine.device_types import Detector as ExEngineDetector
 from exengine.backends.micromanager import MicroManagerCamera as ExEngineMMCamera
+from redsun.toolkit.config import PixelPhotometricTypes
 
 if TYPE_CHECKING:
     import numpy as np
-    from typing import Union
-    from redsun.toolkit.config import DetectorModelInfo, PixelPhotometricTypes
+    from typing import Union, Optional
+    from redsun.toolkit.config import DetectorModelInfo
 
 
 class ExEngineDetectorModel(DetectorModel, ExEngineDetector):
@@ -23,13 +24,14 @@ class ExEngineDetectorModel(DetectorModel, ExEngineDetector):
         name: "str",
         model_info: "DetectorModelInfo",
         exposure: "Union[int, float]",
-        pixel_photometric: "list[PixelPhotometricTypes]" = "gray",
-        bits_per_pixel: "set[int]" = ...,
-        binning: "list[int]" = ...,
-        offset: "Tuple[int, int]" = ...,
-        shape: "Tuple[int, int]" = None,
+        pixel_photometric: "list[PixelPhotometricTypes]" = [PixelPhotometricTypes.GRAY],
+        bits_per_pixel: "Optional[set[int]]" = {8},
+        binning: "Optional[list[int]]" = [1],
+        offset: "Optional[Tuple[int, int]]" = (0, 0),
+        shape: "Optional[Tuple[int, int]]" = None,
     ) -> None:
-        super().__init__(
+        DetectorModel.__init__(
+            self,
             name,
             model_info,
             exposure,
@@ -41,7 +43,7 @@ class ExEngineDetectorModel(DetectorModel, ExEngineDetector):
         )
 
     @abstractmethod
-    def arm(self, frame_count=None) -> None:
+    def arm(self, frame_count : "Optional[int]" = None) -> None:
         """
         Arms the device before an start command. This optional command validates all the current features for
         consistency and prepares the device for a fast start of the Acquisition. If not used explicitly,
@@ -60,7 +62,7 @@ class ExEngineDetectorModel(DetectorModel, ExEngineDetector):
     def is_stopped(self) -> "bool": ...
 
     @abstractmethod
-    def pop_data(self, timeout=None) -> "Tuple[np.ndarray, dict]":
+    def pop_data(self, timeout : Optional[float] = None) -> "Tuple[np.ndarray, dict]":
         """
         Get the next image and metadata from the camera buffer. If timeout is None, this function will block until
         an image is available. If timeout is a number, this function will block for that many seconds before returning
@@ -75,13 +77,14 @@ class ExEngineMMCameraModel(DetectorModel, ExEngineMMCamera):
         name: "str",
         model_info: "DetectorModelInfo",
         exposure: "Union[int, float]",
-        pixel_photometric: "list[PixelPhotometricTypes]" = "gray",
-        bits_per_pixel: set[int] = ...,
-        binning: list[int] = ...,
-        offset: Tuple[int] = ...,
-        shape: Tuple[int] = None,
+        pixel_photometric: "list[PixelPhotometricTypes]" = [PixelPhotometricTypes.GRAY],
+        bits_per_pixel: "Optional[set[int]]" = {8},
+        binning: "Optional[list[int]]" = [1],
+        offset: "Optional[Tuple[int, int]]" = (0, 0),
+        shape: "Optional[Tuple[int, int]]" = None,
     ) -> None:
         DetectorModel.__init__(
+            self,
             name,
             model_info,
             exposure,
