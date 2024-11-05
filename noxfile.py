b546c1757c75ca6badb7dff129c92a763c530ece
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-05 12:04:31 +0100

    Add mypy and test sessions

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,19 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from nox.sessions import Session
 
-@nox.session(venv_backend="mamba", python=["3.9", "3.10", "3.11"])
+python_versions = ["3.9", "3.10", "3.11"]
+
+@nox.session(venv_backend="mamba", python=python_versions)
 def lint(session: "Session") -> None:
     session.conda_install("ruff")
     session.run("ruff", "check")
+
+@nox.session(venv_backend="mamba", python=python_versions)
+def mypy(session: "Session") -> None:
+    session.conda_install("mypy")
+    session.run("mypy", "redsun")
+
+@nox.session(venv_backend="mamba", python=python_versions)
+def test(session: "Session") -> None:
+    session.conda_install("pytest")
+    session.run("pytest", "-v", "tests")
