0555be278e2ae02b8de4558df1d67003a2b81a25
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-05 18:17:39 +0100

    Fix type wrapping

--- src/sunflare/virtualbus.py
+++ src/sunflare/virtualbus.py
@@ -60,7 +60,7 @@ class VirtualBus(ABC, Loggable):
     VirtualBus' signals are implemented using the `psygnal` library; they can be dynamically registered as class attributes, and accessed as a read-only dictionary.
     """
 
-    _signal_registry: Dict[str, Signal] = {}
+    _signal_registry: "Dict[str, Signal]" = {}
 
     def __init__(self) -> None:
         # pre-register signals added as attributes in the class definition
