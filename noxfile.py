451dd4e156ebae9ab660f15a7e098fc63358c71b
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-05 14:00:52 +0100

    Update nox sessions
    - install dependencies

--- noxfile.py
+++ noxfile.py
@@ -4,6 +4,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from nox.sessions import Session
 
+    from typing import List
+
 python_versions = ["3.9", "3.10", "3.11"]
 
 @nox.session(venv_backend="mamba", python=python_versions)
@@ -13,10 +15,18 @@ def lint(session: "Session") -> None:
 
 @nox.session(venv_backend="mamba", python=python_versions)
 def mypy(session: "Session") -> None:
-    session.conda_install("mypy")
-    session.run("mypy", "redsun")
+    requirements : "List[str]" = nox.project.load_toml("pyproject.toml")["project"]["dependencies"]
+    if session.python != "3.9":
+        # typing_extension is built-in from Python 3.10 onwards
+        requirements.remove("typing_extension")
+    session.install(*requirements, "mypy")
+    session.run("mypy", "redsun", "--disable-error-code=import-untyped")
 
 @nox.session(venv_backend="mamba", python=python_versions)
 def test(session: "Session") -> None:
-    session.conda_install("pytest")
+    requirements : "List[str]" = nox.project.load_toml("pyproject.toml")["project"]["dependencies"]
+    if session.python != "3.9":
+        # typing_extension is built-in from Python 3.10 onwards
+        requirements.remove("typing_extension")
+    session.install(*requirements, "pytest")
     session.run("pytest", "-v", "tests")
