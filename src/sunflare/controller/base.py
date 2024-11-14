0648e2f89be42b1d9dfa1ce75dae57598d2f245a
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-14 14:47:19 +0100

    Add `registration_phase` and `connection_phase` APIs
    - allow to have an ordered initialization of default signals and connections

--- src/sunflare/controller/base.py
+++ src/sunflare/controller/base.py
@@ -8,12 +8,11 @@ Two controller types are defined: `DeviceController` and `ComputationalControlle
 
 from abc import ABCMeta, abstractmethod
 from typing import TYPE_CHECKING
+from dataclasses import asdict
 
 from sunflare.config import ControllerInfo
-from sunflare.engine import EngineHandler
 from sunflare.log import Loggable
 from sunflare.utils import create_evented_dataclass
-from sunflare.virtualbus import VirtualBus
 
 if TYPE_CHECKING:
     from typing import Any
@@ -52,31 +51,74 @@ class BaseController(Loggable, metaclass=ABCMeta):
         FullModelInfo = create_evented_dataclass(
             ctrl_info.controllerName + "Info", type(ctrl_info)
         )
-        self._modelInfo = FullModelInfo(**ctrl_info.controllerParams)
+        ctrl_info_dict = asdict(ctrl_info)
+        self._ctrlInfo = FullModelInfo(**ctrl_info_dict)
 
     def shutdown(self) -> None:
         """Shutdown the controller. Performs cleanup operations."""
         ...
 
+    @abstractmethod
+    def registration_phase(self) -> None:
+        """Register the controller signals listed in this method to expose them to the virtual buses.
+
+        At application start-up, controllers can't know what signals are available from other controllers.
+        This method is called after all controllers are initialized to allow them to register their signals.
+        Controllers may be able to register further signals or connect to other controllers' signals even after this phase.
+
+        An implementation example:
+
+        >>> def registration_phase(self) -> None:
+        >>>     # virtual bus signal
+        >>>     self._virtual_bus.sigMySignal = Signal(str, int, **kwargs)
+        >>>
+        >>>     # module bus signal
+        >>>     self._module_bus.sigOtherMySignal = Signal(int, tuple, **kwargs)
+        >>>
+        >>>     # using "register_signal" method
+        >>>     self._virtual_bus.register_signal("sigMySecondOtherSignal", str, **kwargs)
+        """
+        ...
+
+    @abstractmethod
+    def connection_phase(self) -> None:
+        """Connect to other controllers' signals.
+
+        At application start-up, controllers can't know what signals are available from other controllers.
+        This method is invoked after the controller is built and after `registration_phase` as well, allowing to
+        connect to all available registered signals in both virtual buses.
+        Controllers may be able to connect to other controllers' signals even after this phase.
+
+        An implementation example:
+
+        >>> def connection_phase(self) -> None:
+        >>>     # connect to virtual bus signal
+        >>>     self._virtual_bus.sigExternalControllerSignal.connect(self._my_slot)
+        >>>
+        >>>     # connect to module bus signal
+        >>>     self._module_bus.sigOtherExternalControllerSignal.connect(self._my_other_slot)
+        """
+        ...
+
     @property
     def category(self) -> str:
         """Controller category."""
-        return self._modelInfo.category  # type: ignore[no-any-return]
+        return self._ctrlInfo.category  # type: ignore[no-any-return]
 
     @property
     def controllerName(self) -> str:
         """Controller name. Represents the class which builds the controller instance."""
-        return self._modelInfo.controllerName  # type: ignore[no-any-return]
+        return self._ctrlInfo.controllerName  # type: ignore[no-any-return]
 
     @property
     def supportedEngines(self) -> "list[str]":
         """List of supported engines."""
-        return self._modelInfo.supportedEngines  # type: ignore[no-any-return]
+        return self._ctrlInfo.supportedEngines  # type: ignore[no-any-return]
 
     @property
     def controllerParams(self) -> "dict[str, Any]":
         """Controller custom parameters dictionary."""
-        return self._modelInfo.controllerParams  # type: ignore[no-any-return]
+        return self._ctrlInfo.controllerParams  # type: ignore[no-any-return]
 
 
 class DeviceController(BaseController):
@@ -97,10 +139,10 @@ class DeviceController(BaseController):
     @abstractmethod
     def __init__(
         self,
-        ctrl_info: ControllerInfo,
-        handler: EngineHandler,
-        virtual_bus: VirtualBus,
-        module_bus: VirtualBus,
+        ctrl_info: "ControllerInfo",
+        handler: "EngineHandler",
+        virtual_bus: "VirtualBus",
+        module_bus: "VirtualBus",
     ) -> None:
         super().__init__(ctrl_info, handler, virtual_bus, module_bus)
 
@@ -125,9 +167,9 @@ class ComputationalController(BaseController):
     @abstractmethod
     def __init__(
         self,
-        ctrl_info: ControllerInfo,
-        handler: EngineHandler,
-        virtual_bus: VirtualBus,
-        module_bus: VirtualBus,
+        ctrl_info: "ControllerInfo",
+        handler: "EngineHandler",
+        virtual_bus: "VirtualBus",
+        module_bus: "VirtualBus",
     ) -> None:
         super().__init__(ctrl_info, handler, virtual_bus, module_bus)
