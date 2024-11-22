4aaad780aee5b9b875c1021872ee9e801cde3232
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-22 04:34:18 +0100

    Fix linting

--- src/sunflare/engine/bluesky/detector.py
+++ src/sunflare/engine/bluesky/detector.py
@@ -1,8 +1,12 @@
 """Bluesky detector interface."""
 
+from abc import abstractmethod
+
 from typing import TYPE_CHECKING
 
-from bluesky.protocols import Readable, Stageable
+from bluesky.protocols import Readable, Flyable
+
+from ._status import Status
 
 from sunflare.engine import DetectorModel
 
@@ -10,8 +14,26 @@ if TYPE_CHECKING:
     from sunflare.config import DetectorModelInfo
 
 
-class BlueskyDetectorModel(DetectorModel, Readable, Stageable):
+class BlueskyDetectorModel(DetectorModel, Readable, Flyable):
     """Bluesky detector base model."""
 
     def __init__(self, name: str, model_info: "DetectorModelInfo") -> None:
         super().__init__(name, model_info)
+
+    @abstractmethod
+    def stage(self) -> Status:
+        """Set up the device for acquisition.
+
+        It should return a ``Status`` that is marked done when the device is
+        done staging.
+        """
+        ...
+
+    @abstractmethod
+    def unstage(self) -> Status:
+        """Disables device.
+
+        It should return a ``Status`` that is marked done when the device is finished
+        unstaging.
+        """
+        ...
