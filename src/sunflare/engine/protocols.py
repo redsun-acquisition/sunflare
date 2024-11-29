690ac15e8a2978dee80dcfa614fd9d7d3789fd8e
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-29 17:32:15 +0100

    Add generic protocols for handler class
    - a series of protocols to hint implementation in handlers classes

---
+++ src/sunflare/engine/protocols.py
@@ -0,0 +1,50 @@
+# ruff: noqa
+
+from typing import Protocol, TypeVar, Iterator
+
+from sunflare.engine.motor import MotorModel
+from sunflare.engine.light import LightModel
+from sunflare.engine.detector import DetectorModel
+
+# generic dict-like protocol types
+K = TypeVar("K", bound=str)  # key type
+V = TypeVar("V")  # value type
+
+# specific device types
+M = TypeVar("M", bound=MotorModel)
+L = TypeVar("L", bound=LightModel)
+D = TypeVar("D", bound=DetectorModel)
+
+
+class DictLike(Protocol[K, V]):
+    """A dictionary-like protocol."""
+
+    def __getitem__(self, key: K) -> V: ...
+    def __setitem__(self, key: K, value: V) -> None: ...
+    def __delitem__(self, key: K) -> None: ...
+    def __iter__(self) -> Iterator[K]: ...
+    def __len__(self) -> int: ...
+    def keys(self) -> Iterator[K]: ...
+    def values(self) -> Iterator[V]: ...
+    def items(self) -> Iterator[tuple[K, V]]: ...
+
+
+class HasMotors(DictLike[str, M], Protocol):
+    """A dictionary-like protocol that has motors."""
+
+    @property
+    def motors(self) -> DictLike[str, M]: ...
+
+
+class HasLights(DictLike[str, L], Protocol):
+    """A dictionary-like protocol that has lights."""
+
+    @property
+    def lights(self) -> DictLike[str, L]: ...
+
+
+class HasDetectors(DictLike[str, D], Protocol):
+    """A dictionary-like protocol that has detectors."""
+
+    @property
+    def detectors(self) -> DictLike[str, D]: ...
