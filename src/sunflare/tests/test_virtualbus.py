d0cfb743e1e1af9f544ed8f2f974af27c8e05cab
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-01 23:34:19 +0100

    Add test for module bus

--- src/sunflare/tests/test_virtualbus.py
+++ src/sunflare/tests/test_virtualbus.py
@@ -40,3 +40,22 @@ def test_module_virtual_bus() -> None:
     module_bus2 = ModuleVirtualBus()
 
     assert module_bus1 is module_bus2
+
+def test_module_virtual_bus_registration() -> None:
+    """Tests the registration of signals in the module virtual bus."""
+    
+    class MockOwner:
+        sigMySignal = Signal(int, description="My signal")
+    
+    owner = MockOwner()
+    bus = ModuleVirtualBus()
+    
+    bus.register_signals(owner)
+    
+    assert "MockOwner" in bus
+    
+    def test_slot(x: int) -> None:
+        assert x == 5
+    
+    bus["MockOwner"]["sigMySignal"].connect(lambda x: test_slot(x))
+    bus["MockOwner"]["sigMySignal"].emit(5)
