6a13c3f93b2821c8d53a3e7a7d8e018dcf18b94a
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-01 11:16:35 +0100

    Rework AxisLocation
    - remove dependency on bluesky import

--- src/sunflare/types.py
+++ src/sunflare/types.py
@@ -1,22 +1,24 @@
 # noqa: D100
 
-from typing import TYPE_CHECKING, TypeAlias, TypeVar
-
-from bluesky.protocols import Location
+from typing import TYPE_CHECKING, TypeAlias, TypeVar, Generic, TypedDict
 
 if TYPE_CHECKING:
     from typing import Any, Generator, Iterable, Union, Optional
 
-
 T = TypeVar("T")
 
 
-class AxisLocation(Location[T]):
+class AxisLocation(TypedDict, Generic[T]):
     """Extended Location adding axis information."""
 
+    #: The axis along which the Device is moving
     axis: "Optional[Union[str, int]]"
+    #: Where the Device was requested to move to
+    setpoint: T
+    #: Where the Device actually is at the moment
+    readback: T
 
 
 Workflow: TypeAlias = "Union[Generator[Any, None, None], Iterable[Any]]"
 
-__all__ = ["Workflow"]
+__all__ = ["Workflow", "AxisLocation"]
