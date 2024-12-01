ab244b670527186a3da4f2d7c855ef64dbde2255
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-01 12:36:25 +0100

    Make setpoint and readback sequence as well

--- src/sunflare/types.py
+++ src/sunflare/types.py
@@ -10,21 +10,26 @@ X = TypeVar("X", bound=Union[str, int])
 
 
 class AxisLocation(TypedDict, Generic[T, X]):
-    """Typed dictionary for axis-aware device location.
+    r"""Typed dictionary for axis-aware device location.
 
     Parameters
     ----------
     axis : Union[X, Sequence[X]]
-        The axis along which the Device is moving. Can be a single axis or a sequence of axes.
-    setpoint : T
-        Where the Device was requested to move to.
-    readback : T
-        Where the Device actually is at the moment.
+        The axis along which the Device is moving. \
+        Can be a single axis or a sequence of axes.
+    setpoint : Union[T, Sequence[T]]
+        Where the Device was requested to move to. \
+        Can be a single setpoint or a sequence of setpoints. \
+        If it's a sequence, length must match the length of "axis".
+    readback : Union[T, Sequence[T]]
+        Where the Device actually is at the moment. \
+        Can be a single setpoint or a sequence of setpoints. \
+        If it's a sequence, length must match the length of "axis".
     """
 
     axis: "Union[X, Sequence[X]]"
-    setpoint: T
-    readback: T
+    setpoint: "Union[T, Sequence[T]]"
+    readback: "Union[T, Sequence[T]]"
 
 
 Workflow: TypeAlias = "Union[Generator[Any, None, None], Iterable[Any]]"
