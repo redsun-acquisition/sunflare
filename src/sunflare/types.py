1ff4269026ca8f3dec03f4b29b8c7305df84ca5a
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-02 23:16:37 +0100

    Rework axis location

--- src/sunflare/types.py
+++ src/sunflare/types.py
@@ -1,37 +1,28 @@
 # noqa: D100
 
-from typing import TYPE_CHECKING, TypeAlias, TypeVar, Generic, TypedDict
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, TypeAlias, Generic, TypeVar, TypedDict
 
 if TYPE_CHECKING:
-    from typing import Any, Generator, Iterable, Union, Sequence
+    from typing import Any, Generator, Iterable, Union
 
 T = TypeVar("T", bound=Union[int, str, float])
 X = TypeVar("X", bound=Union[str, int])
 
 
-class AxisLocation(TypedDict, Generic[X, T]):
-    r"""Typed dictionary for axis-aware device location.
+class AxisLocation(TypedDict, Generic[T]):
+    """TypedDict for accessing coordinates along a specific axis.
 
     Parameters
     ----------
-    axis : Union[X, Sequence[X]]
-        The axis along which the Device is moving. \
-        Can be a single axis or a sequence of axes.
-    setpoint : Union[T, Sequence[T]]
-        Where the Device was requested to move to. \
-        Can be a single setpoint or a sequence of setpoints. \
-        If it's a sequence, length must match the length of "axis".
-    readback : Union[T, Sequence[T]]
-        Where the Device actually is at the moment. \
-        Can be a single setpoint or a sequence of setpoints. \
-        If it's a sequence, length must match the length of "axis".
+    axis: dict[str, T]
+        A dictionary containing the axis name and its corresponding value.
     """
 
-    axis: "Union[X, Sequence[X]]"
-    setpoint: "Union[T, Sequence[T]]"
-    readback: "Union[T, Sequence[T]]"
+    axis: dict[str, T]
 
 
-Workflow: TypeAlias = "Union[Generator[Any, None, None], Iterable[Any]]"
+Workflow: TypeAlias = Union[Generator[Any, None, None], Iterable[Any]]
 
 __all__ = ["Workflow", "AxisLocation"]
