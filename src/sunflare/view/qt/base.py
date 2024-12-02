f6f793384aa455d2bc58c836e1d2cf4e67497970
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-02 01:05:11 +0100

    Add annotations and better docstring

--- src/sunflare/view/qt/base.py
+++ src/sunflare/view/qt/base.py
@@ -4,6 +4,8 @@ This module contains the basic functionalities a widget of any framework should 
 a Qt GUI that is consistent throught all the RedSun stack.
 """
 
+from __future__ import annotations
+
 from abc import abstractmethod, ABCMeta
 from typing import Protocol, TYPE_CHECKING
 
@@ -25,18 +27,47 @@ class _QMeta(QMetaObject, ABCMeta):
 class WidgetProtocol(Protocol):
     """Minimmal protocol a widget should implement."""
 
-    _virtual_bus: "VirtualBus"
-    _module_bus: "VirtualBus"
+    _virtual_bus: VirtualBus
+    _module_bus: VirtualBus
+
+    @abstractmethod
+    def registration_phase(self) -> None:
+        r"""Register the widget signals listed in this method to expose them to the virtual buses.
+
+        At application start-up, widgets can't know what signals are available from other controllers or widgets. \
+        This method is called after all widgets are initialized to allow them to register their signals. \
+        Widgets may be able to register further signals even after this phase (but not before the `connection_phase` ended). \
+        
+        Only signals defined in your widgets can be registered.
+        
+        An implementation example:
+
+        .. code-block:: python
+
+            def registration_phase(self) -> None:
+                # you can register all signals...
+                self._module_bus.register_signals(self)
+                
+                # ... or only a selection of them
+                self._module_bus.register_signals(self, only=["sigMySignal", "sigMyOtherSignal"])
+                
+                # you can also register signals to the module bus
+                self._module_bus.register_signals(self, only=["sigMySignal", "sigMyOtherSignal"])
+        """
+        ...
+
+    @abstractmethod
+    def connection_phase(self) -> None: ...
 
     @property
     @abstractmethod
-    def virtual_bus(self) -> "VirtualBus":
+    def virtual_bus(self) -> VirtualBus:
         """Returns the inter-module bus."""
         ...
 
     @property
     @abstractmethod
-    def module_bus(self) -> "VirtualBus":
+    def module_bus(self) -> VirtualBus:
         """Returns the intra-module bus."""
         ...
 
@@ -59,21 +90,21 @@ class BaseWidget(QWidget, WidgetProtocol, metaclass=_QMeta):
     @abstractmethod
     def __init__(
         self,
-        virtual_bus: "VirtualBus",
-        module_bus: "VirtualBus",
-        *args: "Any",
-        **kwargs: "Any",
+        virtual_bus: VirtualBus,
+        module_bus: VirtualBus,
+        *args: Any,
+        **kwargs: Any,
     ) -> None:
         super().__init__(*args, **kwargs)
         self._virtual_bus = virtual_bus
         self._module_bus = module_bus
 
     @property
-    def virtual_bus(self) -> "VirtualBus":
+    def virtual_bus(self) -> VirtualBus:
         """Returns the inter-module bus."""
         return self._virtual_bus
 
     @property
-    def module_bus(self) -> "VirtualBus":
+    def module_bus(self) -> VirtualBus:
         """Returns the intra-module bus."""
         return self._module_bus
