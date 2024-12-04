9a4e8b106024de39539efb87511ccd6ec8134644
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-04 11:39:32 +0100

    Add buffer type for image data with embedded metadata

--- src/sunflare/types.py
+++ src/sunflare/types.py
@@ -2,10 +2,12 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, TypeAlias, Generic, TypeVar, TypedDict
+from typing import TYPE_CHECKING, Generic, TypeVar, TypedDict
 
 if TYPE_CHECKING:
-    from typing import Any, Generator, Iterable, Union
+    from typing import Any, Generator, Iterable, Union, Tuple, TypeAlias
+
+    import numpy.typing as npt
 
 T = TypeVar("T", bound=Union[int, str, float])
 X = TypeVar("X", bound=Union[str, int])
@@ -24,5 +26,7 @@ class AxisLocation(TypedDict, Generic[T]):
 
 
 Workflow: TypeAlias = Union[Generator[Any, None, None], Iterable[Any]]
+
+Buffer: TypeAlias = dict[str, Tuple[npt.NDArray[Any], dict[str, Any]]]
 
 __all__ = ["Workflow", "AxisLocation"]
