5193f9c9d80d35c4ae41f904fe501779af223b9b
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-11 20:12:43 +0100

    Hide handler protocol class
    - use it directly in redsun;
    - caused problems in plugin system

--- src/sunflare/engine/__init__.py
+++ src/sunflare/engine/__init__.py
@@ -1,8 +1,7 @@
 # noqa: D104
 
 from .detector import DetectorModel
-from .handler import EngineHandler
 from .light import LightModel
 from .motor import MotorModel
 
-__all__ = ["EngineHandler", "DetectorModel", "MotorModel", "LightModel"]
+__all__ = ["DetectorModel", "MotorModel", "LightModel"]
