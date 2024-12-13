c5277f0792784fbbe06399f394a24bb0e325bb3c
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-13 22:23:14 +0100

    Remove config parameter from device registry
    - not needed

--- src/sunflare/engine/bluesky/registry.py
+++ src/sunflare/engine/bluesky/registry.py
@@ -2,14 +2,13 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, TypeAlias, cast
+from typing import TYPE_CHECKING, TypeAlias
 
 from sunflare.engine.registry import DeviceRegistry
 
 if TYPE_CHECKING:
-    from typing import Union, Literal
+    from typing import Union
 
-    from sunflare.config import RedSunInstanceInfo
     from sunflare.virtualbus import VirtualBus
 
     from .motor import BlueskyMotorModel
@@ -23,26 +22,13 @@ class BlueskyDeviceRegistry(DeviceRegistry[BlueskyMotorModel, BlueskyDetectorMod
 
     def __init__(
         self,
-        config: RedSunInstanceInfo,
         virtual_bus: VirtualBus,
         module_bus: VirtualBus,
     ) -> None:
-        self._config = config
         self._virtual_bus = virtual_bus
         self._module_bus = module_bus
         self._motors: dict[str, BlueskyMotorModel] = {}
         self._detectors: dict[str, BlueskyDetectorModel] = {}
-
-    def registration_phase(self) -> None:  # noqa: D102
-        # nothing to do
-        ...
-
-    def connection_phase(self) -> None:  # noqa: D102
-        # sigNewDevices is defined in the HardwareVirtualBus class,
-        # which is defined in the Redsun package;
-        # it is not accessed via mypy in order to avoid circular imports;
-        # this signal is built-in and not exposed to other users so it's safe to ignore
-        self._virtual_bus.sigNewDevices.connect(self._on_new_devices)  # type: ignore
 
     @property
     def motors(self) -> dict[str, BlueskyMotorModel]:
@@ -53,22 +39,3 @@ class BlueskyDeviceRegistry(DeviceRegistry[BlueskyMotorModel, BlueskyDetectorMod
     def detectors(self) -> dict[str, BlueskyDetectorModel]:
         """Get the detectors dictionary."""
         return self._detectors
-
-    def _on_new_devices(
-        self, group: Literal["motors", "detectors"], devices: Registry
-    ) -> None:
-        """Add the newly created devices to the respective dictionaries.
-
-        Parameters
-        ----------
-        group : str
-            The group of the devices.
-        devices : dict[str, dict[str, str]]
-            The dictionary of newly built devices.
-        """
-        if group == "motors":
-            self._motors.update(cast(dict[str, BlueskyMotorModel], devices))
-        elif group == "detectors":
-            self._detectors.update(cast(dict[str, BlueskyDetectorModel], devices))
-        else:
-            raise ValueError(f"Invalid group: {group}")
--- src/sunflare/engine/exengine/registry.py
+++ src/sunflare/engine/exengine/registry.py
@@ -1,13 +1,12 @@
 """ExEngine device registry module."""
 
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING
 
 from sunflare.engine.registry import DeviceRegistry
 
 if TYPE_CHECKING:
-    from typing import Union, TypeAlias, Literal
+    from typing import Union, TypeAlias
 
-    from sunflare.config import RedSunInstanceInfo
     from sunflare.virtualbus import VirtualBus
 
     from .motor import (
@@ -34,26 +33,13 @@ class ExEngineDeviceRegistry(DeviceRegistry[ExEngineMotor, ExEngineDetector]):
 
     def __init__(
         self,
-        config: RedSunInstanceInfo,
         virtual_bus: VirtualBus,
         module_bus: VirtualBus,
     ) -> None:
-        self._config = config
         self._virtual_bus = virtual_bus
         self._module_bus = module_bus
         self._motors: dict[str, ExEngineMotor] = dict()
         self._detectors: dict[str, ExEngineDetector] = dict()
-
-    def registration_phase(self) -> None:  # noqa: D102
-        # nothing to do
-        ...
-
-    def connection_phase(self) -> None:  # noqa: D102
-        # sigNewDevices is defined in the HardwareVirtualBus class,
-        # which is declared in the Redsun package;
-        # it is not accessed in order to avoid circular imports;
-        # this signal is built-in and not exposed to other users so it's safe to ignore
-        self._virtual_bus.sigNewDevices.connect(self._on_new_devices)  # type: ignore
 
     @property
     def motors(self) -> dict[str, ExEngineMotor]:
@@ -64,22 +50,3 @@ class ExEngineDeviceRegistry(DeviceRegistry[ExEngineMotor, ExEngineDetector]):
     def detectors(self) -> dict[str, ExEngineDetector]:
         """Get the detectors dictionary."""
         return self._detectors
-
-    def _on_new_devices(
-        self, group: Literal["motors", "detectors"], devices: Registry
-    ) -> None:
-        """Add the newly created devices to the respective dictionaries.
-
-        Parameters
-        ----------
-        group : str
-            The group of the devices.
-        devices : dict[str, dict[str, str]]
-            The dictionary of newly built devices.
-        """
-        if group == "motors":
-            self._motors.update(cast(dict[str, ExEngineMotor], devices))
-        elif group == "detectors":
-            self._detectors.update(cast(dict[str, ExEngineDetector], devices))
-        else:
-            raise ValueError(f"Invalid group: {group}")
