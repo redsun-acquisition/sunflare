8de304b23b8283910682f5283b1964dcc1cb4a6f
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-22 14:07:19 +0100

    Add version management

--- src/sunflare/__init__.py
+++ src/sunflare/__init__.py
@@ -1,2 +1,7 @@
 # noqa: D104
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("sunflare")
+except PackageNotFoundError:
+    __version__ = "unknown"
