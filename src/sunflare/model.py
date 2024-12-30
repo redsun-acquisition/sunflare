6899706013404e96f96dd34cc15c57d0befcee0d
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-30 19:13:56 +0100

    Add Configurable protocol to minimal protocol

--- src/sunflare/model.py
+++ src/sunflare/model.py
@@ -1,7 +1,7 @@
 """Models represent device interfaces that Bluesky can interact with.
 
-Models are "providers", meaning that they offer a subset of Bluesky protocols to execute actions.
-A minimal recognizable device in RedSun must implement the :class:`~sunflare.model.DeviceProtocol` protocol.
+Models offer a subset of Bluesky protocols to execute actions.
+A minimal recognizable device in RedSun must implement the :class:`~sunflare.model.ModelProtocol` protocol.
 """
 
 from __future__ import annotations
@@ -11,6 +11,8 @@ from typing import TYPE_CHECKING, Protocol, runtime_checkable
 
 if TYPE_CHECKING:
     from typing import Any, Optional
+
+    from bluesky.protocols import DataKey, Reading, SyncOrAsync
 
     from sunflare.config import ModelInfo
 
@@ -25,16 +27,59 @@ class ModelProtocol(Protocol):
 
     - :class:`~bluesky.protocols.HasName`
     - :class:`~bluesky.protocols.HasParent`
+    - :class:`~bluesky.protocols.Configurable`
     """
 
     @abstractmethod
     def __init__(self, name: str, model_info: ModelInfo) -> None: ...
 
+    @abstractmethod
+    def configure(self, name: str, value: Any) -> None:
+        """Configure the model.
+
+        Parameters
+        ----------
+        name : ``str``
+            The name of the configuration parameter to set.
+        value : ``Any``
+            The value to set for the configuration parameter.
+        """
+        ...
+
+    @abstractmethod
+    def read_configuration(self) -> SyncOrAsync[dict[str, Reading]]:
+        """Read the model configuration.
+
+        Provides a dictionary with the current values of the model configuration.
+
+        Returns
+        -------
+        ``dict[str, Reading]``
+            A dictionary with the current values of the model configuration.
+        """
+        ...
+
+    @abstractmethod
+    def describe_configuration(self) -> SyncOrAsync[dict[str, DataKey]]:
+        """Describe the model configuration.
+
+        Provides a description of each field of the model configuration.
+
+        Returns
+        -------
+        ``dict[str, DataKey]``
+            A dictionary with the description of each field of the model configuration.
+        """
+        ...
+
     @property
     @abstractmethod
     def name(self) -> str:
-        """Used to populate object_keys in the Event DataKey.
+        """Model identifier.
 
+        Used to populate object_keys in the Event DataKey.
+
+        See the following link for more information:
         https://blueskyproject.io/event-model/event-descriptors.html#object-keys
         """
         ...
@@ -53,6 +98,6 @@ class ModelProtocol(Protocol):
     def model_info(self) -> ModelInfo:
         """The object associated model information.
 
-        Must be a subclass of :class:`~sunflare.config.DeviceModelInfo`.
+        Must be a subclass of :class:`~sunflare.config.ModelInfo`.
         """
         ...
