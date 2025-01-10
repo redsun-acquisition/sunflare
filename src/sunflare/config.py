e78dcacf6c4bbdc17908ac27c60dbb0d20428a02
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-01-10 18:22:57 +0100

    Add validation

--- src/sunflare/config.py
+++ src/sunflare/config.py
@@ -132,11 +132,23 @@ class MotorInfo(ModelInfo):
         Engineering unit for motor position.
     """
 
-    axis: list[str] = field(
-        validator=validators.instance_of(str), on_setattr=setters.frozen
-    )
-    step_size: float = field(validator=validators.instance_of(float))
+    axis: list[str] = field(factory=list, on_setattr=setters.frozen)
+    step_size: dict[str, float] = field(factory=dict)
     egu: str = field(validator=validators.instance_of(str), on_setattr=setters.frozen)
+
+    @axis.validator
+    def _validate_axis(self, _: str, value: list[str]) -> None:
+        if not all(isinstance(val, str) for val in value):
+            raise ValueError("All values in the list must be strings.")
+        if len(value) == 0:
+            raise ValueError("The list must contain at least one element.")
+
+    @step_size.validator
+    def _validate_step_size(self, _: str, value: dict[str, float]) -> None:
+        if not all(isinstance(val, float) for val in value.values()):
+            raise ValueError("All values in the dictionary must be floats.")
+        if len(value) == 0:
+            raise ValueError("The dictionary must contain at least one element.")
 
 
 @define(kw_only=True)
