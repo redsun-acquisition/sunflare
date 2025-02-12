af7a06c3bd2948c02d97bfc4e2942316307b0a47
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-02-12 17:08:11 +0100

    General formatting
    - thanks ruff

--- docs/conf.py
+++ docs/conf.py
@@ -9,11 +9,11 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path('..', 'src').resolve()))
+sys.path.insert(0, str(Path("..", "src").resolve()))
 
-project = 'SunFlare'
-copyright = '2024, Jacopo Abramo'
-author = 'Jacopo Abramo'
+project = "SunFlare"
+copyright = "2024, Jacopo Abramo"
+author = "Jacopo Abramo"
 
 github_user = "redsun-acquisition"
 github_repo = "sunflare"
@@ -22,33 +22,33 @@ github_repo = "sunflare"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
     "sphinx.ext.githubpages",
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autosummary',
-    'sphinxcontrib.mermaid',
-    'sphinx_design',
-    'myst_parser'
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinxcontrib.mermaid",
+    "sphinx_design",
+    "myst_parser",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build']
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
 
 intersphinx_mapping = {
-    'bluesky': ('https://blueskyproject.io/bluesky/main', None),
-    'event_model': ('https://blueskyproject.io/event-model/main', None),
+    "bluesky": ("https://blueskyproject.io/bluesky/main", None),
+    "event_model": ("https://blueskyproject.io/event-model/main", None),
 }
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
 html_context = {
-   # this doesn't really matter;
-   # adding it only for completion
-   "default_mode": "auto"
+    # this doesn't really matter;
+    # adding it only for completion
+    "default_mode": "auto"
 }
 
 html_theme_options = {
@@ -59,23 +59,23 @@ html_theme_options = {
             "icon": "fa-brands fa-square-github",
             "type": "fontawesome",
         }
-   ]
+    ]
 }
 
 mermaid_version = "11.4.0"
 myst_fence_as_directive = ["mermaid"]
-myst_enable_extensions = ['attrs_block', 'colon_fence']
+myst_enable_extensions = ["attrs_block", "colon_fence"]
 
 html_css_files = [
-    'custom.css',
+    "custom.css",
 ]
 
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
 
 napoleon_numpy_docstring = True
-autodoc_typehints = 'description'
+autodoc_typehints = "description"
 
 myst_heading_anchors = 3
