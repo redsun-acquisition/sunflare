6705e358683012018dba245472cae45dd19a9814
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-02-13 23:38:59 +0100

    Add preliminary work to integrate msgspec

---
+++ src/sunflare/engine/_utils.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import zmq
+from typing import Literal, Union
+from itertools import count
+from weakref import WeakValueDictionary
+from enum import Enum
+
+
+class DocumentNames(str, Enum):
+    """Mirror of event_model.DocumentNames, subclassed from str.
+
+    Deprecated events are omitted.
+    """
+
+    stop = "stop"
+    start = "start"
+    descriptor = "descriptor"
+    event = "event"
+    datum = "datum"
+    resource = "resource"
+    event_page = "event_page"
+    datum_page = "datum_page"
+    stream_resource = "stream_resource"
+    stream_datum = "stream_datum"
+
+
+class SocketRegistry:
+    def __init__(self) -> None:
+        self._allowed_sigs = DocumentNames
+        self._sockets: WeakValueDictionary[int, zmq.SyncSocket] = WeakValueDictionary()
+        self._token_count = count()
+
+    def connect(
+        self, sig: Union[Literal["all"], DocumentNames], socket: zmq.SyncSocket
+    ) -> None:
+        if sig not in self._allowed_sigs and sig != "all":
+            raise ValueError(f"Signal '{sig}' is not allowed.")
+        token = next(self._token_count)
+        self._sockets[token] = socket
+
+    def disconnect(self, token: int) -> None:
+        self._sockets.pop(token)
+
+    def process(self, sig: DocumentNames) -> None:
+        for socket in self._sockets.values():
+            socket.send_string(sig)
