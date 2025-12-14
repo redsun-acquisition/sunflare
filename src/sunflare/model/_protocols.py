17a151290b9de13392444faaa70666b554d800ee
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-12-14 18:06:17 +0100

    fix: make ModelProtocol inherit directly from bluesky protocols and remove init function
    - this could probably be enforced via instancecheck

--- src/sunflare/model/_protocols.py
+++ src/sunflare/model/_protocols.py
@@ -1,22 +1,21 @@
 from __future__ import annotations
 
 from abc import abstractmethod
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
+from bluesky.protocols import Configurable, HasName, HasParent
 from typing_extensions import Protocol, runtime_checkable
 
 if TYPE_CHECKING:
-    from typing import Any
-
-    from bluesky.protocols import Descriptor, Reading, SyncOrAsync
-
     from sunflare.config import ModelInfoProtocol
 
 __all__ = ["ModelProtocol"]
 
 
 @runtime_checkable
-class ModelProtocol(Protocol):  # pragma: no cover
+class ModelProtocol(
+    HasName, HasParent, Configurable[Any], Protocol
+):  # pragma: no cover
     """Minimal required protocol for a recognizable device in Redsun.
 
     Exposes the following Bluesky protocols:
@@ -24,69 +23,7 @@ class ModelProtocol(Protocol):  # pragma: no cover
     - :class:`~bluesky.protocols.HasName`
     - :class:`~bluesky.protocols.HasParent`
     - :class:`~bluesky.protocols.Configurable`
-
-    Parameters
-    ----------
-    name : ``str``
-        Name of the model. Serves as a unique identifier for the object created from it.
-    model_info : :class:`~sunflare.config.ModelInfoProtocol`
-        Object implementing :class:`~sunflare.config.ModelInfoProtocol`.
     """
-
-    @abstractmethod
-    def __init__(self, name: str, model_info: ModelInfoProtocol) -> None: ...
-
-    @abstractmethod
-    def read_configuration(self) -> SyncOrAsync[dict[str, Reading[Any]]]:
-        """Read the model configuration.
-
-        Provides a dictionary with the current values of the model configuration.
-
-        The method can be normal or `async`.
-
-        Returns
-        -------
-        dict[``str``, :class:`~bluesky.protocols.Reading`]
-            A dictionary with the current values of the model configuration.
-        """
-        ...
-
-    @abstractmethod
-    def describe_configuration(self) -> SyncOrAsync[dict[str, Descriptor]]:
-        """Describe the model configuration.
-
-        Provides a description of each field of the model configuration.
-
-        The method can be normal or `async`.
-
-        Returns
-        -------
-        dict[``str``, :class:`~bluesky.protocols.Descriptor`]
-            A dictionary with the description of each field of the model configuration.
-        """
-        ...
-
-    @property
-    @abstractmethod
-    def name(self) -> str:
-        """Model identifier.
-
-        Used to populate object_keys in :class:`~event_model.DataKey`.
-
-        See the following link for more information:
-        https://blueskyproject.io/event-model/main/explanations/event-descriptors.html
-        """
-        ...
-
-    @property
-    @abstractmethod
-    def parent(self) -> Any | None:
-        """``None``, or a reference to a parent device.
-
-        Used by the RE to stop duplicate stages.
-        In Redsun (for now) should always return ``None``.
-        """
-        ...
 
     @property
     @abstractmethod
