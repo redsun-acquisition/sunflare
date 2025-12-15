f407fef082ea22c800d4aa5c3855e3c198ef1ec0
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-12-15 12:01:29 +0100

    refactor: remove the possibility to emit documents over zmq from the engine
    - bad design choice, the consumers have to deal with that

--- src/sunflare/engine/_wrapper.py
+++ src/sunflare/engine/_wrapper.py
@@ -14,13 +14,10 @@ from bluesky.run_engine import RunEngineResult
 from bluesky.utils import DuringTask, RunEngineInterrupted
 from event_model.documents import Document
 
-from sunflare.virtual import encode
-
 if TYPE_CHECKING:
     from typing import Iterable
 
     from bluesky.utils import Msg, Subscribers
-    from event_model import DocumentNames
 
 __all__ = ["RunEngine", "RunEngineResult", "RunEngineInterrupted", "Document"]
 
@@ -32,16 +29,6 @@ _prefix_counter = count()
 
 class RunEngine(BlueskyRunEngine):
     """Subclass of ``bluesky.run_engine.RunEngine`` to allow execution in a separate thread.
-
-    Additional features:
-
-    - ``socket``: A ZMQ socket to send messages to a remote endpoint;
-    - ``socket_prefix``: A prefix to be used in the ZMQ topic when sending messages.
-
-    When launching a plan, the ``RunEngine``, the ``__call__`` method returns a ``Future`` object.
-    This allows to set a callback on the future to retrieve the result of the execution.
-    Alternatively, the result can be accessed directly from the ``result`` attribute
-    when the future is done.
 
     Suppressed features:
 
@@ -57,15 +44,6 @@ class RunEngine(BlueskyRunEngine):
       does not interact with any possible event loop in the main thread.
 
     For the original class initializer signature, refer to the :class:`~bluesky.run_engine.RunEngine` documentation.
-
-    Parameters
-    ----------
-    socket_prefix : ``str``, keyword-only, optional
-        Prefix to be used in the ZMQ topic when sending messages.
-        Default is ``RE{N}``, where ``{N}`` is a counter that increments for each instance.
-    socket : ``zmq.Socket[bytes]``, keyword-only, optional
-        ZMQ socket to send messages to a remote endpoint.
-        Default is ``None`` (no messages are sent).
     """
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
@@ -74,34 +52,12 @@ class RunEngine(BlueskyRunEngine):
         # SignalHandler context manager
         kwargs["context_managers"] = []
         kwargs["during_task"] = DuringTask()
-        self.socket_prefix: str = kwargs.pop(
-            "socket_prefix", f"RE{next(_prefix_counter)}"
-        )
-        self.socket: zmq.Socket[bytes] | None = kwargs.pop("socket", None)
         self._executor = ThreadPoolExecutor(max_workers=1)
         self._result: REResultType
         super().__init__(*args, **kwargs)
 
         # override pause message to be an empty string
         self.pause_msg = ""
-
-    def emit_sync(self, name: DocumentNames, doc: dict[str, Any]) -> None:
-        """Emit a document synchronously.
-
-        Reimplemented to send documents also through a ZMQ socket.
-
-        .. warning::
-
-            This method is not meant to be used directly.
-            The ``RunEngine`` will emit documents automatically
-            during the execution of a plan. Any
-            subscriber will receive the documents.
-
-        """
-        if self.socket is not None:
-            topic = f"{self.socket_prefix}/{str(name).split('.')[-1]}"
-            self.socket.send_multipart([topic.encode(), encode(doc)])
-        super().emit_sync(name, doc)
 
     def __call__(  # type: ignore[override]
         self,
