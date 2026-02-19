576e0e55db709e91b3bb809c460c9c46bef58295
Jacopo Abramo <jacopo.abramo@gmail.com>
2026-02-19 17:26:16 +0100

    refactor: return the ViewPositionTypes enum (now ViewPosition) back to SDK
    - moved (wrongly) to container layer
    - declared as read-only attribute of View base class (and protocol)

--- src/sunflare/view/__init__.py
+++ src/sunflare/view/__init__.py
@@ -1,3 +1,36 @@
+from enum import Enum, unique
+
 from ._base import PView, View
 
 __all__ = ["PView", "View"]
+
+
+@unique
+class ViewPosition(str, Enum):
+    """Supported view positions.
+
+    Used to define the position of a view component in the main view of the UI.
+
+    !!! warning
+        These values are based on how Qt manages dock widgets.
+        They may change in the future.
+
+    Attributes
+    ----------
+    CENTER : str
+        Center view position.
+    LEFT : str
+        Left view position.
+    RIGHT : str
+        Right view position.
+    TOP : str
+        Top view position.
+    BOTTOM : str
+        Bottom view position.
+    """
+
+    CENTER = "center"
+    LEFT = "left"
+    RIGHT = "right"
+    TOP = "top"
+    BOTTOM = "bottom"
